e7362d09fcd904afad5a4adda1f728120ffb1ae3
lpm <leodanypm@gmail.com>
2026-08-10 16:44:40 -0400

    app accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('lector', 'Lector'),
+        ('colaborador', 'Colaborador'),
+        ('revisor', 'Revisor'),
+        ('administrador', 'Administrador'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='lector')
+
+    def __str__(self):
+        return self.username
--- final_project/settings.py
+++ final_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig', #
 ]
+AUTH_USER_MODEL = 'accounts.CustomUser' #
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'final_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], #
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] #
+LOGIN_REDIRECT_URL = "home" #
+LOGOUT_REDIRECT_URL = "home" #
